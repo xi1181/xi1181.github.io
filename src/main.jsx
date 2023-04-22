81a4071443e7ee293828a67550b05dd75e858a38
Jackimaru96 <jackchenyujie.96@gmail.com>
2023-04-22 18:09:56 +0800

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import Splash from './components/splash/index';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import App from './App';
+// import Splash from './components/splash/index';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Splash />} />
+//         <Route path="/home" element={<App />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/home" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
