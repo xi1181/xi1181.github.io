4537af9a4b6daa7cd718ffed01aafbc0924811ce
Jackimaru96 <jackchenyujie.96@gmail.com>
2023-04-22 18:18:01 +0800

    Update

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -187,7 +187,7 @@ const config = {
   class="text-primary" href="https://github.com/arifszn/gitprofile"
   target="_blank"
   rel="noreferrer"
->GitProfile</a> & <div class="flex items-center"><img width="20" height="20" src="./public/TYM.png"/> <a class="text-primary" href="https://theyoungmaker.com/" target="_blank" rel="noreferrer">The Young Maker </a></div>`,
+>GitProfile</a> & <div class="flex items-center"><img width="20" height="20" src="TYM.png"/> <a class="text-primary" href="https://theyoungmaker.com/" target="_blank" rel="noreferrer">The Young Maker </a></div>`,
 };
 
 export default config;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import App from './App';
-// import Splash from './components/splash/index';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Splash />} />
-//         <Route path="/home" element={<App />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Splash from './components/splash/index';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/home" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
